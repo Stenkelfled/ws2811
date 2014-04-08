baa984450bf2c3407d54882b2d4dd1ec0cbfdd95
Stenkelfled <github@t-immermann.de>
2014-04-08 13:44:02 +0200

    * clock and io settings are now working

--- V1.0/src/main.c
+++ V1.0/src/main.c
@@ -26,66 +26,65 @@
  */
 #include <asf.h>
 #include <Leds.h>
-#include <avr/io.h>
+#include <util/delay.h>
 
-void usart_init(){
-	USARTC1.BAUDCTRLA = 8; //BSEL = 8 --> 1,6667 MHz
-	USARTC1.CTRLA |= USART_DREINTLVL_LO_gc;
-    USARTC1.CTRLB |= USART_TXEN_bm;
-	USARTC1.CTRLC |= USART_CMODE_MSPI_gc | (1<<1); //Master SPI Mode | lsb first
-}
+void clock_init(void);
 
-void dma_init(){
-	DMA.CTRL = 0<<DMA_ENABLE_bp | 0<<DMA_RESET_bp | DMA_DBUFMODE_DISABLED_gc | DMA_PRIMODE_CH0123_gc;
-	DMA.CH0.CTRLA |= DMA_CH_SINGLE_bm | DMA_CH_BURSTLEN_8BYTE_gc;
-	DMA.CH0.CTRLA &= ~(DMA_CH_REPEAT_bm);
-	DMA.CH0.ADDRCTRL |= DMA_CH_SRCRELOAD_TRANSACTION_gc | DMA_CH_SRCDIR_INC_gc | DMA_CH_DESTRELOAD_BURST_gc | DMA_CH_DESTDIR_FIXED_gc;
-	DMA.CH0.TRIGSRC = DMA_CH_TRIGSRC_USARTC1_DRE_gc;
-	DMA.CH0.TRFCNT = LED_COUNT*BYTES_PER_LED;
-	DMA.CH0.REPCNT = 1;
-}
+void clock_init(void){
+    //Setup: calibrate 32MHz osc as 48MHz for USB
+    //use 2MHz osc with PLL(mul = 10) as 20MHz CPU and peripheral clock
+    //use for USART in SPI mode: BSEL=2 -> Baudrate=3.333333MHz ->1Bit = 300ns -> 4Bit per Color-bit
 
-void fill_buffer(uint8_t src, uint8_t* dest){
-    for (uint8_t i=0;i<8;i++){
-        /*if(!(i%2)){
-            dest[i/2] = 0;
-        }*/
-        if(src&(1<<i)){
-            dest[i/2] |= LED_CODE_1<<(4*(i%2));
-        } else {
-            dest[i/2] |= LED_CODE_0<<(4*(i%2));
-        }
-    }
+    OSC.CTRL |= OSC_RC32MEN_bm; //enable 32MHz oscillator
+    while(!(OSC.STATUS&OSC_RC32MRDY_bm)){} //wait for 32MHz oscillator to be ready
+    
+    //set up the DFLL for 32MHz calibration
+    uint16_t osc_rc32m_comp = 0xB71B;
+    DFLLRC32M.COMP1 = (osc_rc32m_comp >> 0)&0xFF;
+    DFLLRC32M.COMP2 = (osc_rc32m_comp >> 8)&0xFF;
+    OSC.DFLLCTRL = OSC_RC32MCREF_RC32K_gc;
+    OSC.CTRL |= OSC_RC32KEN_bm; //enable 32kHz oscillator as reference oscillator
+    while(!(OSC.STATUS&OSC_RC32KRDY_bm)){} // wait for 32kHz oscillator to be ready
+    DFLLRC32M.CTRL |= DFLL_ENABLE_bm; // enable DFLL
+
+    CLK.USBCTRL = CLK_USBPSDIV_1_gc | CLK_USBSRC_RC32M_gc; //select 32MHz (48Mhz) oscillator as USB clock source
+
+    //configure PLL: 
+        //Input = 2MHz osc
+        //Multiplier = 10 -> 20MHz
+    OSC.PLLCTRL = OSC_PLLSRC_RC2M_gc | (10)<<OSC_PLLFAC_gp;
+    OSC.CTRL |= OSC_PLLEN_bm; //enable PLL
+    while(!(OSC.STATUS&OSC_PLLRDY_bm)){} // wait for PLL to be ready
+
+    ccp_write_io((void*)&CLK.PSCTRL,CLK_PSADIV_1_gc | CLK_PSBCDIV_1_1_gc); //Prescalers: No division for peripherals
+    ccp_write_io((void*)&CLK.CTRL,CLK_SCLKSEL_PLL_gc); //Select PLL output as System Clock source (should be 20MHz)
 }
 
 int main (void)
 {
-	sysclk_init();
+    clock_init();
 	board_init();
 	
-    //!!CAUTION: Check, whether the registers of USART and DMA are really set - in the simulation they weren't.
+	while(1){
+		_delay_ms(500);
+		LED_RD_ON
+		_delay_ms(500);
+		LED_RD_OFF
+	}
 
-	usart_init();
-	dma_init();
+/*    define_group(0,0,2,1);
+    define_group(1,3,7,2);
+    append_to_group(0,4);
 
-    uint8_t red = 0xFF;
-    uint8_t green = 0xF2;
-    uint8_t blue = 0x0F;
-    led_data_t led_data = {0,0,0};
+    rgbcolor_t clr={0x11,0x11,0x88};
+    color_group(0,clr);
+    clr.red=0xFF;clr.green=0x0F;clr.blue=0x00;
+    color_group(1,clr);
 
-    fill_buffer(red,led_data.red);
-    fill_buffer(green,led_data.green);
-    fill_buffer(blue,led_data.blue);
+    fill_buffer();
 
-	DMA.CH0.SRCADDR0 = (((uint16_t)&led_data) >> 0 )&0xFF;
-	DMA.CH0.SRCADDR1 = (((uint16_t)&led_data) >> 8 )&0xFF;
-	DMA.CH0.SRCADDR2 = 0;
+    while(1){}
 
-	DMA.CH0.DESTADDR0 = (((uint16_t)&USARTC1.DATA) >> 0 )&0xFF;
-	DMA.CH0.DESTADDR1 = (((uint16_t)&USARTC1.DATA) >> 8 )&0xFF;
-	DMA.CH0.DESTADDR2 = 0;
-
-    DMA.CH0.CTRLA |= DMA_CH_ENABLE_bm;
-
-	return 0;
+*/
+	// Insert application code here, after the board has been initialized.
 }
